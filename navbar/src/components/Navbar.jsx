aa1fa8e88516d106f7bda5b942355e2e52361540
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-11-17 23:03:47 +0700

    update styled navbar

--- navbar/src/components/Navbar.jsx
+++ navbar/src/components/Navbar.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Logo } from "./Logo";
-
+import {
+  NavLinkWrapper,
+  NavbarWrapper,
+  StyledNavLink,
+  StyledFontAwesomeIcon,
+} from "../styles/Navbar.styled";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 export const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const link = [
+    {
+      page: "Home",
+      href: "/",
+    },
+    { page: "About", href: "/about" },
+    { page: "Blog", href: "/blog" },
+    {
+      page: "Services",
+      href: "/services",
+    },
+    { page: "Sign Up", href: "/sign-up" },
+  ];
   return (
-    <div>
+    <NavbarWrapper>
       <Logo />
-    </div>
+      <StyledFontAwesomeIcon icon={faBars} onClick={() => setActive(!active)} />
+      <NavLinkWrapper active={active}>
+        {link.map((link) => (
+          <StyledNavLink
+            activeclassname="active"
+            key={link.page}
+            to={link.href}
+          >
+            {link.page}
+          </StyledNavLink>
+        ))}
+      </NavLinkWrapper>
+    </NavbarWrapper>
   );
 };
