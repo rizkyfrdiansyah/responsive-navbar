038f72875a2d993d6ac8567e31db966eae30c4e8
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-11-17 23:04:50 +0700

    update logo

--- navbar/src/components/Logo.jsx
+++ navbar/src/components/Logo.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { LogoImg } from "../styles/Navbar.styled";
 import logo from "../assets/logo.svg";
-
 export const Logo = () => {
   return <LogoImg src={logo} alt="logo" />;
 };
